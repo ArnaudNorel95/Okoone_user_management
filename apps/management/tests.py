aa11a4c25e02ae61b68be93c139c0e40a22dc45e
ArnaudNorel95 <arnaudnorel@yahoo.fr>
2023-09-23 21:00:02 +0200

    test customers list and customer detail

--- apps/management/tests.py
+++ apps/management/tests.py
@@ -10,7 +10,7 @@ from datetime import datetime
 class CustomerModelTest(TestCase):
     def test_user_creation(self):
         """
-            Ensure Customer is working - Database Side.
+            Ensure Customers are working - Database Side.
         """
         employee = Employees.objects.create(
             first_name  = "Adams",
@@ -59,6 +59,37 @@ class CustomerApiTest(TestCase):
             hire_date   = "2003-05-03 00:00:00",
             email       = "adamsandrew@yahoo.fr"
         )
+
+        customer_a = Customers.objects.create(
+            first_name  = "testa",
+            last_name   = "testa",
+            company     = "Testa",
+            address     = "12th 68 Nyc",
+            city        = "NYC",
+            state       = "NY state",
+            country     = "USA",
+            postal_code = "4512",
+            phone       = "+3346489",
+            fax         = "+3367891",
+            email       = "testa@yahoo.fr",
+            support_employee = employee
+        )
+
+        customer_b = Customers.objects.create(
+            first_name  = "testb",
+            last_name   = "testb",
+            company     = "Testb",
+            address     = "12th 68 Nyc",
+            city        = "NYC",
+            state       = "NY state",
+            country     = "USA",
+            postal_code = "4512",
+            phone       = "+3346489",
+            fax         = "+3367891",
+            email       = "testb@yahoo.fr",
+            support_employee = employee
+        )
+
         self.sample_customer_data = {
             "first_name":"Arnaud",
             "last_name":"Norel",
@@ -74,12 +105,79 @@ class CustomerApiTest(TestCase):
             "support_employee":employee.id
         }
 
-        self.url = reverse("customers-list")    
+        self.url_list = reverse("customers-list")
+        self.url_detail = reverse("customers-detail", kwargs={'pk': 1}) 
+
+
+    def test_read_customers(self):
+        """
+            Ensure we can get Customer list - API side
+        """
+        response = self.client.get(self.url_list, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Customers.objects.count(), 2)
+
+        last_customer = Customers.objects.last()
+        self.assertEqual(last_customer.first_name, 'testb')
+        self.assertEqual(last_customer.last_name, 'testb')
+        self.assertEqual(last_customer.company, 'Testb')
+        self.assertEqual(last_customer.address, '12th 68 Nyc')
+        self.assertEqual(last_customer.city, 'NYC')
+        self.assertEqual(last_customer.state, 'NY state')
+        self.assertEqual(last_customer.country, 'USA')
+        self.assertEqual(last_customer.postal_code, '4512')
+        self.assertEqual(last_customer.phone, '+3346489')
+        self.assertEqual(last_customer.fax, '+3367891')
+        self.assertEqual(last_customer.email, 'testb@yahoo.fr')
+        self.assertEqual(last_customer.support_employee.last_name, 'Andrew')
+
+
+    def test_read_specific_customer(self):
+        """
+            Ensure we can get 1 specific Customer - API side
+        """
+        response = self.client.get(self.url_detail, format='json')
+        customer_a_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(customer_a_data['first_name'], 'testa')
+        self.assertEqual(customer_a_data['last_name'], 'testa')
+        self.assertEqual(customer_a_data['company'], 'Testa')
+        self.assertEqual(customer_a_data['address'], '12th 68 Nyc')
+        self.assertEqual(customer_a_data['city'], 'NYC')
+        self.assertEqual(customer_a_data['state'], 'NY state')
+        self.assertEqual(customer_a_data['country'], 'USA')
+        self.assertEqual(customer_a_data['postal_code'], '4512')
+        self.assertEqual(customer_a_data['phone'], '+3346489')
+        self.assertEqual(customer_a_data['fax'], '+3367891')
+        self.assertEqual(customer_a_data['email'], 'testa@yahoo.fr')
+        self.assertEqual(customer_a_data['support_employee'], 1)        
 
     def test_create_customer(self):
         """
             Ensure we can't create a new Customer according to the ReadOnly Viewset- API side
+            Firstly we remove the customers added in database in precedent test, to have an empty table
         """
-        response = self.client.post(self.url, data=self.sample_customer_data, format='json')
+        Customers.objects.all().delete()
+        response = self.client.post(self.url_list, data=self.sample_customer_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         self.assertEqual(Customers.objects.count(), 0)
+
+class ArtistModelTest(TestCase):
+    def test_user_creation(self):
+        """
+            Ensure Artists is working - Database Side.
+        """
+        artist = Artists.objects.create(
+            name  = "50 Cent"
+        )
+
+        self.assertEqual(artist.name, "50 Cent")
+
+class ArtistApiTest(TestCase):
+    def setup(self):
+        self.client = APIClient()
+        self.sample_artist_data = {
+            "name": "Eminem"
+        }
